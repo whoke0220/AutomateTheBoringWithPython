6bedb582a381dd45f0fc47d707d268a66871bed5
William Hoke <hokewilliam0@gmail.com>
2021-01-30 12:15:25 -0800

    Added the Chapter03 folder and the magic8ball program

---
+++ Chapter03/magic8ball.py
@@ -0,0 +1,27 @@
+#Turns your computer into a magic 8 ball!
+
+import random
+
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain.'
+    elif answerNumber == 2:
+        return 'It is decidedly so.'
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'Reply hazy. Try again.'
+    elif answerNumber == 5:
+        return 'Ask again later'
+    elif answerNumber == 6:
+        return 'Concentrate and ask again.'
+    elif answerNumber == 7:
+        return 'My reply is no.'
+    elif answerNumber == 8:
+        return 'Outlook is not so good.'
+    elif answerNumber ==9:
+        return 'Very doubtful.'
+
+
+r = random.randint(1,9)
+print(getAnswer(r))
