dbe34918f432b9d0c54b6927f6288e591b5c6191
William Hoke <hokewilliam0@gmail.com>
2021-01-30 15:59:41 -0800

    Added CollatzSequence program

---
+++ Chapter03/CollatzSequence.py
@@ -0,0 +1,14 @@
+print("The Collatz sequence performs a series of mathematical operations on")
+print("a number until it arrives at 1. The operation taken on the number is")
+print("determined on whether or not the current number in the sequence is even or odd.")
+print("Enter a number and I will show you: ")
+userNumber = int(input())
+currentNumber = userNumber
+
+while currentNumber != 1:
+    if currentNumber % 2 == 0:
+        print(str(currentNumber // 2))
+        currentNumber = currentNumber // 2
+    else:
+        print(str(currentNumber * 3 + 1))
+        currentNumber = currentNumber * 3 + 1
