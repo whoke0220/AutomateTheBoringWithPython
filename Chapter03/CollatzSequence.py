855c92d6cbd950817e8a7ad8880132f383addecf
William Hoke <hokewilliam0@gmail.com>
2021-01-30 16:04:03 -0800

    Added input validation to CollatzSequence

--- Chapter03/CollatzSequence.py
+++ Chapter03/CollatzSequence.py
@@ -2,7 +2,15 @@ print("The Collatz sequence performs a series of mathematical operations on")
 print("a number until it arrives at 1. The operation taken on the number is")
 print("determined on whether or not the current number in the sequence is even or odd.")
 print("Enter a number and I will show you: ")
-userNumber = int(input())
+
+correctInput = True
+while correctInput:
+    try:
+        userNumber = int(input())
+        correctInput = False
+    except ValueError:
+        print('Please enter an integer:')
+
 currentNumber = userNumber
 
 while currentNumber != 1:
