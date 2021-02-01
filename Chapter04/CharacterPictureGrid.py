4cbfd0aa99b0c441e059a8a1a1dc4f4604ca2900
William Hoke <hokewilliam0@gmail.com>
2021-01-31 23:03:28 -0800

    Finish chapter 4. Added CharacterPictureGrid program.

---
+++ Chapter04/CharacterPictureGrid.py
@@ -0,0 +1,16 @@
+#Prints a grid of characters to make a picture
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for y in range(len(grid[0]) - 1):
+    for x in range(len(grid) - 1):
+        print(grid[x][y], end='')
+    print()
