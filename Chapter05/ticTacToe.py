ca3bc56c56e0f91cc2573e39f3a70cd44f2d16c3
whoke0220 <hokewilliam0@gmail.com>
2021-02-04 15:22:00 -0800

    Added win checking. Still needs to verify valid moves and no overwriting already made moves.

--- Chapter05/ticTacToe.py
+++ Chapter05/ticTacToe.py
@@ -4,7 +4,7 @@ theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
             'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 
-win = False
+GameWin = False
 winningPlayer = ''
 
 def printBoard(board):
@@ -15,53 +15,55 @@ def printBoard(board):
     print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
 def checkForWin(board):
+    print('Checking for winner.')
+    global winningPlayer
     #Top row winner
-    if board['top-L'] == board['top-M'] and board['top-L'] == board['top-R']:
-        win = True
+    if board['top-L'] != ' ' and board['top-L'] == board['top-M'] and board['top-L'] == board['top-R']:
         winningPlayer = board['top-L']
+        return True
 
     #Left most column winner
-    elif board['top-L'] == board['mid-L'] and board['top-L'] == board['low-L']:
-        win = True
+    elif board['top-L'] != ' ' and board['top-L'] == board['mid-L'] and board['top-L'] == board['low-L']:
         winningPlayer = board['top-L']
+        return True
 
     #Diagonal top-to-bottom, left-to-right winner
-    elif board['top-L'] == board['mid-M'] and board['top-L'] == board['low-R']:
-        win = True
+    elif board['top-L'] != ' ' and board['top-L'] == board['mid-M'] and board['top-L'] == board['low-R']:
         winningPlayer = board['top-L']
+        return True
 
     #middle vertical winner
-    elif board['top-M'] == board['mid-M'] and board['top-M'] == board['low-M']:
-        win = True
+    elif board['top-M'] != ' ' and board['top-M'] == board['mid-M'] and board['top-M'] == board['low-M']:
         winningPlayer = board['top-M']
+        return True
 
     #right most column winner
-    elif board['top-R'] == board['mid-R'] and board['top-R'] == board['low-R']:
-        win = True
+    elif board['top-R'] != ' ' and board['top-R'] == board['mid-R'] and board['top-R'] == board['low-R']:
         winningPlayer = board['top-R']
+        return True
 
     #middle row winner
-    elif board['mid-L'] == board['mid-M'] and board['mid-L'] == board['mid-R']:
-        win = True
+    elif board['mid-L'] != ' ' and board['mid-L'] == board['mid-M'] and board['mid-L'] == board['mid-R']:
         winningPlayer = board['mid-L']
+        return True
 
     #bottom row wimmer
-    elif board['low-L'] == board['low-M'] and board['low-L'] == board['low-R']:
-        win = True
-        winningPlayer = board['low-L']
+    elif board['low-L'] != ' ' and board['low-L'] == board['low-M'] and board['low-L'] == board['low-R']:
+        winningPlayer = board['top-L']
+        return True
 
     #diagonal left to right, top to bottom winner
-    elif board['low-L'] == board['mid-M'] and board['low-L'] == board['top-R']:
-        win = True
-        winningPlayer = board['low-L']
+    elif board['low-L'] != ' ' and board['low-L'] == board['mid-M'] and board['low-L'] == board['top-R']:
+        winningPlayer = board['top-L']
+        return True
 
     else:
-        win = False
+        return False
 
 turn = 'X'
 numTurns = 0
 
-while win != True or numTurns < 9:
+while GameWin != True:
     printBoard(theBoard)
     print('Turn for ' + turn + '. Move on which space?')
     move = input()
@@ -74,9 +76,12 @@ while win != True or numTurns < 9:
     else:
         turn = 'X'
     numTurns += 1
-    checkForWin(theBoard)
+    if numTurns >= 4:
+        GameWin = checkForWin(theBoard)
 
-if win == True:
+if GameWin == True:
     print('The winner is: ' + winningPlayer)
+    printBoard(theBoard)
 else:
     print('No one wins!')
+    printBoard(theBoard)
