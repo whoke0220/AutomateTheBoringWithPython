d4211b562b84318ef717d10390c1567aeb1ff117
William Hoke <hokewilliam0@gmail.com>
2021-05-02 11:27:40 -0700

    added list of pieces for both players

--- Chapter05/ChessBoardValidator.py
+++ Chapter05/ChessBoardValidator.py
@@ -1,2 +1,4 @@
 def isValidChessBoard(chessBoard):
-    
+
+black_pieces = ['b_pawn', 'b_king', 'b_queen', 'b_bishop', 'b_rook', 'b_knight']
+white_pieces = ['w_pawn', 'w_king', 'w_queen', 'w_bishop', 'w_rook', 'w_knight']
