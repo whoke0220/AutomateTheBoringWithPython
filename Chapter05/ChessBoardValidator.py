60f63eee70db3687cf29a1627e3392ff11f8eb60
William Hoke <hokewilliam0@gmail.com>
2021-05-05 15:27:46 -0700

    continued work on ChessBoardValidator.py

--- Chapter05/ChessBoardValidator.py
+++ Chapter05/ChessBoardValidator.py
@@ -1,4 +1,16 @@
-def isValidChessBoard(chessBoard):
+# Variables for max number of pieces a player can have and the max number of
+# pawns each player can have.
+max_number_of_pieces = 16
+max_number_of_pawns = 8
 
-black_pieces = ['b_pawn', 'b_king', 'b_queen', 'b_bishop', 'b_rook', 'b_knight']
-white_pieces = ['w_pawn', 'w_king', 'w_queen', 'w_bishop', 'w_rook', 'w_knight']
+def isValidChessBoard(chessBoard):
+    #Set initial counter variables to 0. Will be used to track the number of
+    #pieces in each subset.
+    number_of_black_kings = 0
+    number_of_white_kings = 0
+    number_of_black_pawns = 0
+    number_of_white_pawns = 0
+    number_of_black_pieces = 0
+    number_of_white_pieces = 0
+
+    for piece in chessBoard.values():
